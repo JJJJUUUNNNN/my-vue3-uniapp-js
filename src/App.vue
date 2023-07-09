a5525ff6fe63ca6ee39d531b6fa57d6a4521653b
JJJJUUUNNNN <1902958347@qq.com>
2023-07-10 07:19:31 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  onLaunch: function () {
+    console.log('App Launch')
+  },
+  onShow: function () {
+    console.log('App Show')
+  },
+  onHide: function () {
+    console.log('App Hide')
+  },
+}
+</script>
+
+<style lang="scss">
+/*每个页面公共css */
+// 设置整个项目的背景色
+	page {
+		background-color: #F5F7FC;
+	}
+
+</style>
